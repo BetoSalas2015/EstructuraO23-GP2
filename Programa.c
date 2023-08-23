f4c4971f6bccd97db426633fc99a327997159fc3
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2023-08-23 17:22:27 -0600

    Sesión 05 - 23/08/2023

--- Programa.c
+++ Programa.c
@@ -1,33 +1,15 @@
-#include <stdio.h>
+#include <stdio.h>			//  FILE se declara aquí
 #include <stdlib.h>
 
 #define pausa system("pause")
 
-
 int main()
 {
-	const float PI = 3.14159;
-	int r = 50;
-	float area;
-	area = PI * r * r; 
-	//  Conversión y formato
-	printf("'%c' y %d base 8 = %o, base 16 = %x \n", r, r, r, r);
+	FILE *archivo;
 
-	printf("%f y %e\n", area, area);
-          
-	printf("El 50%% de r es %f\n", (r*0.5) );
+	archivo = fopen("archivo.txt", "w");
 
-	printf("Aqui va una cadena => %s \n", "Saludos Programas!!!");
-
-	//  Ancho de Campo
-	printf("%f y %e\n", area, area);
-	printf("%15f y %15e\n", area, area);
-	printf("%15.3f y %15.3e\n", area, area);
-
-	// Banderas
-	printf("%015.3f y %e\n", area, area);
-	printf("%+15.3f y %e\n", -area, area);
-	printf("%-15.3f y %e\n", area, area);
+	fclose(archivo);
 
 	pausa;
 	return 0;
