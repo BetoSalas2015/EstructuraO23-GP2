0e40bdb03d284284174302fc4ae08093257a728b
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2023-09-06 17:12:36 -0600

    Sesión 11 - 06/09/2023

--- Programa.c
+++ Programa.c
@@ -8,6 +8,7 @@ struct nombre_persona
 {
 	char nombre[15];
 	char apellido[15];
+
 };
 
 struct persona 
@@ -21,13 +22,13 @@ struct persona
 int main()
 {
 	int costo;
-	struct persona miPersona;
+	struct persona agenda[5];
 
-	miPersona.edad = 25;
- 	miPersona.altura = 1.65;
-	miPersona.sexo = 'H';
-	//strcpy(  miPersona.nombre, "Juan Perez" );
 
+	agenda[3].edad = 25;    
+	agenda[2].altura = 1.65;
+	agenda[4].sexo = 4;
+	
 	
 	pausa;
 	return 0;
